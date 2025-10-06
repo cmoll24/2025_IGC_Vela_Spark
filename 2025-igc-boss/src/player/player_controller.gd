8977c7781ba1cf8f8f676da0d9d0f44f7ab4ec52
Christophe Moll <115204696+cmoll24@users.noreply.github.com>
2025-10-06 15:37:52 -0700

    Added coyote time, jump grace period and debug line

--- 2025-igc-boss/src/player/player_controller.gd
+++ 2025-igc-boss/src/player/player_controller.gd
@@ -1,41 +1,87 @@
 extends CharacterBody2D
 
+@onready var jump_grace_timer = $JumpGraceTime
+@onready var coyote_timer = $CoyoteTime
+
 @export_category("Movement variables")
-@export var MOVE_SPEED = 500.0
-@export var H_DECELERATION = 10
-@export var GRAVITY = 1000.0
+@export var MOVE_SPEED : float = 500.0
+@export var H_DECELERATION : float = 10
+@export var GRAVITY : float = 1000.0
+@export var MAX_FALL_SPEED : float = 50.0
 
 @export_category("Jump variables")
-@export var JUMP_SPEED = 900.0
-@export var MAX_JUMP_AMOUNT = 2
-var jump_amount = MAX_JUMP_AMOUNT
+@export var JUMP_SPEED : float = 800.0
+@export var MAX_AIR_JUMP_AMOUNT : int = 1
+var air_jump_amount : int = MAX_AIR_JUMP_AMOUNT
 
 var move_input = Vector2.ZERO
+var current_gravity = GRAVITY
+var current_move_speed = MOVE_SPEED
+
+var is_touching_floor = false
 
 func _physics_process(delta: float) -> void:
-	velocity.y += GRAVITY * delta
+	if velocity.y < 20 and velocity.y > -20 and not is_on_floor(): 
+		current_gravity = GRAVITY / 2
+	elif velocity.y > 0:
+		current_gravity = GRAVITY * 2.5
+	else:
+		current_gravity = GRAVITY
+	
+	velocity.y += min(current_gravity * delta, MAX_FALL_SPEED)
 	horizontal_movement(delta)
 	jump(delta)
 	
 	move_and_slide()
+	
+	if is_on_floor():
+		is_touching_floor = true
+	elif is_touching_floor:
+		is_touching_floor = false
+		coyote_timer.start()
+	
+	$Debug_Label.text = "Gravity: {grav}".format({'grav' : current_gravity / 100})
 
 func horizontal_movement(delta):
 	move_input = Input.get_axis("move_left", "move_right")
 	
 	if move_input:
-		velocity.x = move_input * MOVE_SPEED
+		velocity.x = move_input * current_move_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, delta * H_DECELERATION * MOVE_SPEED)
+		velocity.x = move_toward(velocity.x, 0, delta * H_DECELERATION * current_move_speed)
 
 func jump(_delta):
-	if is_on_floor():
-		jump_amount = MAX_JUMP_AMOUNT
-		
-	if Input.is_action_just_pressed("jump") and jump_amount > 0:
-		jump_amount -= 1
-		velocity.y = -JUMP_SPEED
+	if can_player_jump():
+		air_jump_amount = MAX_AIR_JUMP_AMOUNT
+		if is_jump_just_pressed():
+			coyote_timer.stop()
+			velocity.y = -JUMP_SPEED
 	
-	if not is_on_floor():
-		if Input.is_action_just_released("jump") and velocity.y < 0: #add extra gravity
-			velocity.y = lerp(velocity.y, GRAVITY, 0.2)
-			velocity.y *= 0.3
+	else:
+		if air_jump_amount > 0:
+			if is_jump_just_pressed():
+				air_jump_amount -= 1
+				velocity.y = -JUMP_SPEED
+			
+	if not is_on_floor() and Input.is_action_just_released("jump") and velocity.y < 0: #add extra gravity
+		#velocity.y = 0
+		velocity.y = lerp(velocity.y, 0.0, 0.8)
+		#velocity.y = lerp(velocity.y, GRAVITY, 0.2)
+		#velocity.y *= 0.3
+
+func is_jump_just_pressed():
+	if not jump_grace_timer.is_stopped():
+		jump_grace_timer.stop()
+		return true
+	return false
+
+func can_player_jump():
+	if is_on_floor():
+		return true
+	elif not coyote_timer.is_stopped():
+		return true
+	return false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		jump_grace_timer.start()
