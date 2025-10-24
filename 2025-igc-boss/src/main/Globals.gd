3ccd40af96ea0efd008ab33a3bdad7ce4f2d9aa6
Christophe Moll <115204696+cmoll24@users.noreply.github.com>
2025-10-24 13:08:11 -0700

    Improved functions to get player

--- 2025-igc-boss/src/main/Globals.gd
+++ 2025-igc-boss/src/main/Globals.gd
@@ -11,3 +11,9 @@ func get_current_level_index():
 func switch_to_level(new_index : int):
 	current_level_index = new_index
 	get_tree().change_scene_to_file("res://src/main/game.tscn")
+
+func get_game_scene():
+	return game_scene
+
+func get_player():
+	return game_scene.current_level.get_player()
