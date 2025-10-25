694a46e429c9fc19f86803b0ce918d22dd63ab2e
Christophe Moll <115204696+cmoll24@users.noreply.github.com>
2025-10-24 20:45:33 -0700

    Added level 10

--- 2025-igc-boss/src/main/title.gd
+++ 2025-igc-boss/src/main/title.gd
@@ -6,14 +6,18 @@ extends Control
 var current_level : Level
 
 func _ready() -> void:
-	var number_of_levels = 9
+	var number_of_levels = 10
+	var buttons = []
 	for i in range(1,number_of_levels+1):
-		var level_button = level_selector.instantiate()
+		var level_button : LevelSelector = level_selector.instantiate()
 		level_button.set_level_number(i)
 		level_container.add_child(level_button)
+		buttons.append(level_button)
 		
-		if i == 1:
-			level_button.grab_focus()
+		
+	buttons[0].grab_focus()
+	buttons[0].focus_neighbor_top = buttons[-1].get_path()
+	buttons[-1].focus_neighbor_bottom = buttons[0].get_path()
 
 func get_level():
 	return current_level
