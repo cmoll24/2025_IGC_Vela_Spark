53c3842d1d74a4a7e6dc57195c1a7325acfbe72c
Icicle333 <Liammccabe2006@outlook.com>
2025-11-01 18:26:28 -0700

    Droplet now does damage, spawner work in progress
    Rain does damage, continuously spawning still in progress

--- 2025-igc-boss/fallingProjectile.gd
+++ 2025-igc-boss/fallingProjectile.gd
@@ -1,4 +1,19 @@
 extends Area2D
 
+var isReady = true
+var projectile
+@onready var level = get_tree().current_scene
+var starting_x : float
+
 func _process(delta: float) -> void:
 	position.y += 100 * delta
+
+
+
+
+
+
+	
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body.hit(self)
