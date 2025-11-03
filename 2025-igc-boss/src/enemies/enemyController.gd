919f6424e32f0bc7c926fcd010d4e5fee80b2ce5
Christophe Moll <115204696+cmoll24@users.noreply.github.com>
2025-11-03 11:38:29 -0800

    Enemies die to spikes

--- 2025-igc-boss/src/enemies/enemyController.gd
+++ 2025-igc-boss/src/enemies/enemyController.gd
@@ -5,12 +5,13 @@ extends CharacterBody2D
 @export var move_time : float = 3.0
 @export var speed : float = 200.0
 @export var activation_delay: float = 0.1
+@export var MAX_HEALTH = 1
 
 var direction = -1
 var timer = 0.0
 var active = false
 
-var health = 1
+var health = MAX_HEALTH
 
 var projectile
 
@@ -38,8 +39,9 @@ func _physics_process(_delta: float) -> void:
 #		summon_projectile()
 #		cooldown = 5.0
 
-func collide(_body: Node2D) -> void:
-	pass
+func collide(body: Node2D) -> void:
+	if body.is_in_group("obstacles"):
+		take_damage(MAX_HEALTH)
 	#if body is Player:
 	#	body.damage(self)
 
