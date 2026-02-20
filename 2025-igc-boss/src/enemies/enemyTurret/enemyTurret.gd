252c44ed3c0276f4fa58cd84946f87c9f6793057
Christophe Moll <115204696+cmoll24@users.noreply.github.com>
2026-02-19 18:51:48 -0800

    Added blank turret enemy animation

--- 2025-igc-boss/src/enemies/enemyTurret/enemyTurret.gd
+++ 2025-igc-boss/src/enemies/enemyTurret/enemyTurret.gd
@@ -2,7 +2,8 @@ extends Enemy
 class_name EnemyTurret
 
 @onready var animation_player = $AnimationPlayer
-@onready var animated_sprite = $rotate/AnimatedSprite2D2
+@onready var animated_sprite = $rotate/AnimatedSprite2D
+@onready var mouth_animation = $rotate/MouthAnimation
 
 @export var COOLDOWN = 3
 
@@ -18,14 +19,14 @@ func _physics_process(delta: float) -> void:
 	
 	if global_position.distance_squared_to(player_pos) < 3_000_000:
 		cooldown_timer -= delta
-		if cooldown_timer < 1:
+		if cooldown_timer < 0.8:
 			animation_player.play("shoot")
 		if cooldown_timer < 0:
 			cooldown_timer = COOLDOWN
 			summon_projectile(player_pos)
-			animated_sprite.play("idle")
-		if cooldown_timer < 1.4:
-			animated_sprite.play("attack")
+			mouth_animation.play("idle")
+		if cooldown_timer < 1:
+			mouth_animation.play("shoot")
 	
 func summon_projectile(target_pos : Vector2) -> void:
 	var instance : StraightProjectile = projectile.instantiate()
